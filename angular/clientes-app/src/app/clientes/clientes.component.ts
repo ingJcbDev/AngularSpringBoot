d57d235def2a34327e228235ba7850db48834060
ingJcbDev <ing.jcb.dev@gmail.com>
2022-09-14 16:59:48 -0500

    17. Creando el componente clientes.component

---
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent {
+
+  constructor() {}
+
+}
