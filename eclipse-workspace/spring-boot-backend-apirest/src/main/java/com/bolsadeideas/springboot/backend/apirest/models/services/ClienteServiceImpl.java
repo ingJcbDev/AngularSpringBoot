af917c7aa4edd489d698df65d7f8aa6de0a547d9
ingJcbDev <ing.jcb.dev@gmail.com>
2023-03-22 20:24:25 -0500

    38. Añadiendo las clases Repository y Service de la lógica de negocio38. Añadiendo las clases Repository y Service de la lógica de negocio

---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+@Service // Decora la clase como componente Service
+public class ClienteServiceImpl implements IClienteService{
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true) //Transacion de solo lectura aunque esto no es necesario
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+}
