8f87f67b76b4f471c11d76c0b8942771283117d6
ingJcbDev <ing.jcb.dev@gmail.com>
2023-03-23 19:43:42 -0500

    40. Añadiendo Datos de pueba

---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* populate table clientes */
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres0', 'Guzman', 'andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres1', 'Guzman1', '1andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres2', 'Guzman2', '2andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres3', 'Guzman3', '3andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres4', 'Guzman4', '4andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres5', 'Guzman5', '5andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres6', 'Guzman6', '6andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres7', 'Guzman7', '7andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres8', 'Guzman8', '8andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres9', 'Guzman9', '9andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres10', 'Guzman10', '10andres.guzman@gmail.com', '2018-01-01');
+INSERT INTO `clientes` (nombre, apellido, email, create_at) VALUES('Andres11', 'Guzman11', '11andres.guzman@gmail.com', '2018-01-01');
